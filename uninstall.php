5317a32fcd66de152c205e514f2cb52567fd73a6
Hasanuzzaman shozib <webhasan24@gmail.com>
2019-12-31 18:05:05 +0600

    Update uninstall hook

--- uninstall.php
+++ uninstall.php
@@ -1,10 +1,10 @@
 <?php 
 /**
- * Accomplish Uninstall / Delete woocommerce prodcut recommend 
+ * Accomplish Uninstall / Delete woocommerce prodcut recommend data
  * 
  * @since      1.0.0
 */
 
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit; // Exit if accessed directly
 
-delete_post_meta_by_key( 'pgfy_pr_data' ); 
+delete_post_meta_by_key( 'pgfy_pr_data' );  // remove all data 
