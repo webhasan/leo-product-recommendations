fc51043502fe13f15524f553d939841c9ce69a19
Hasanuzzaman shozib <webhasan24@gmail.com>
2019-12-31 15:13:34 +0600

    Added all feature of modal product

--- includes/template-modal.php
+++ includes/template-modal.php
@@ -21,36 +21,34 @@
             </div>
             <div class="wpr-modal-body">
                 <div class="recommended-product-list">
+
                 <?php
-                $args = array(
-                    'post_type' => 'product',
-                    'posts_per_page' => -1,
-                    'post__in' => $selectedPostsId,
-                );
+                    $args = array(
+                        'post_type' => 'product',
+                        'posts_per_page' => -1,
+                        'post__in' => $selectedPostsId,
+                    );
+
                     $loop = new WP_Query( $args );
                         if ( $loop->have_posts() ): while ( $loop->have_posts() ) : $loop->the_post();
                     ?>
-
+                    
                     <div class="single-wpr">
-                        <a href="<?php the_permalink(); ?>" style="display: block;">
-                            <?php the_post_thumbnail(); ?>
-                            <?php the_title(); ?>
-                        </a>
-
+                        <a href="<?php the_permalink(); ?>"> 
                         <?php 
-                            echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
-                            sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
-                                esc_url( $product->add_to_cart_url() ),
-                                esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-                                esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
-                                isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-                                esc_html( $product->add_to_cart_text() )
-                            ),
-                            $product, $args );
+                            do_action( 'woocommerce_before_shop_loop_item_title' );
+                            do_action( 'woocommerce_shop_loop_item_title' );     
+                            do_action( 'woocommerce_after_shop_loop_item_title' );               
                         ?>
+                        </a>
+                        <?php echo do_shortcode('[add_to_cart 
+                            id="'.get_the_ID().'" 
+                            show_price = "FALSE"
+                            style=""
+                        ]'); ?>
                     </div>
                     
-                    <?php endwhile; endif; wp_reset_postdata();?>
+                    <?php endwhile;  wp_reset_postdata(); endif; ?>
                 </div>
             </div>
 
