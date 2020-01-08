4a75f74bcb39c29716e3b92881e2bf5a894a0e62
Hasanuzzaman shozib <webhasan24@gmail.com>
2020-01-08 14:23:50 +0600

    Fixed modal heading singular/plural product

--- templates/template-modal.php
+++ templates/template-modal.php
@@ -15,7 +15,8 @@
     $common_heading = apply_filters(
         'wpr_common_heading',
         sprintf(
-            __('<h2 class="modal-heading">You may purchase following products with "%s"</h2>','woocommerce-product-recommend'), 
+            __('<h2 class="modal-heading">You may purchase following %1$s with "%2$s"</h2>','woocommerce-product-recommend'), 
+            _n( 'product', 'products', $total_rc_products, 'woocommerce-product-recommend' ),
             get_the_title($product_id)
         )
     );
