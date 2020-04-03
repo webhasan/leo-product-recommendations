30b0692ebb018be5c8393938e15a11a36983039c
Md Hasanuzzaman <webhasan24@gmail.com>
2020-04-03 10:42:39 +0600

    Edit uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -7,11 +7,9 @@
 
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit; // Exit if accessed directly
 
-add_action('admin_init', function() {
-	$all_plugins = get_plugins();
-    $check_free_version = array_key_exists('woocommerce-product-recommend-pro/woocommerce-product-recommend-pro.php', $all_plugins);
-    
-    if(!$check_free_version) {
-        delete_post_meta_by_key( '_pgfy_pr_data' );  // remove all data 
-    }
-});
+$all_plugins = get_plugins();
+$check_pro_version = array_key_exists('woocommerce-product-recommend-pro/woocommerce-product-recommend-pro.php', $all_plugins);
+
+if(!$check_pro_version) {
+    delete_post_meta_by_key( '_pgfy_pr_data' );  // remove all data 
+}
