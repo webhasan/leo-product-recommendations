f37facff25190ce36efbefc3786462338ba8e819
webhasan <webhasan24@gmail.com>
2022-03-31 19:28:30 +0600

    Fiexed typo

--- includes/page-tutorials.php
+++ includes/page-tutorials.php
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
     <h2><?php _e('How To Setup','leo-product-recommendations'); ?></h2>
     <div class="tutorials-wrapper">
         <h3><?php _e('Basic Setup:', 'leo-product-recommendations');?></h3>
-        <div class="tutorial-video"><iframe style="max-with: 100%" width="1000" height="560" src="https://www.youtube.com/embed/zKUoISoDj94" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <div class="tutorial-video"><iframe style="max-width: 100%" width="1000" height="560" src="https://www.youtube.com/embed/zKUoISoDj94" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
         <h3><a href="https://cutt.ly/KjE8vEF" target="_blank"><?php _e('See Details Documentation','leo-product-recommendations') ?></a></h3>
     </div>
 </div>
