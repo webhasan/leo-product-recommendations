225229922037a5c2b25d0bc4036008cfcd4ed7b5
webhasan <webhasan24@gmail.com>
2022-05-31 07:37:18 +0600

    Update WPEditor component

--- assets/js/components/WPEditor.js
+++ assets/js/components/WPEditor.js
@@ -1,45 +1,44 @@
-const { useEffect, useState } = wp.element;
+const { useEffect } = wp.element;
 
-const WPEditor = ({ onChange, id, ...options}) => {
-  const [loaded, setLoaded] = useState(false);
+const initWPEditor = (id) => {
+	setTimeout(() => {
+		let { tinymce: tinymceObj, quicktags: quicktagsObj } =
+			wp.editor.getDefaultSettings();
+		wp.editor.initialize(id, {
+			tinymce: {
+				...tinymceObj,
+				toolbar1:
+					"formatselect,,forecolor,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_add_media,wp_adv",
+				toolbar2:
+					"strikethrough,hr,pastetext,removeformat,charmap,outdent,indent,undo,redo",
+				plugins:
+					"charmap,colorpicker,hr,lists,media,paste,tabfocus,textcolor,fullscreen,wordpress,wpautoresize,wpeditimage,wpemoji,wpgallery,wplink,wpdialogs,wptextpattern,wpview",
 
-  useEffect(function () {
-    if (window.tinymce && wp && wp.editor && wp.editor.getDefaultSettings && !loaded) {
-      setLoaded(true);
-      let {
-        tinymce: tinymceObj,
-        quicktags: quicktagsObj,
-      } = wp.editor.getDefaultSettings();
-      
-      wp.editor.initialize(id, {
-        tinymce: {
-          ...tinymceObj,
-          toolbar1:
-            "formatselect,,forecolor,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,wp_add_media,wp_adv",
-          toolbar2:
-            "strikethrough,hr,pastetext,removeformat,charmap,outdent,indent,undo,redo",
-          plugins:
-            "charmap,colorpicker,hr,lists,media,paste,tabfocus,textcolor,fullscreen,wordpress,wpautoresize,wpeditimage,wpemoji,wpgallery,wplink,wpdialogs,wptextpattern,wpview",
+				setup: function (editor) {
+					editor.on("NodeChange", function (e) {
+						editor.save();
+					});
+				},
+			},
+			quicktags: quicktagsObj,
+		});
+	}, 400);
+};
 
-          setup: function (editor) {
-            editor.on("NodeChange", function (e) {
-              editor.save();
-            });
-          }
-        },
-        quicktags: quicktagsObj,
-      });
-    }
-  });
+const WPEditor = ({ onChange, id, ...options }) => {
+	useEffect(function () {
+		document.addEventListener("DOMContentLoaded", () => initWPEditor(id));
+		// window.addEventListener("load", () => initWPEditor(id));
+	}, []);
 
-  return (
-      <textarea
-        style={{height: 200}}
-        {...options}
-        id={id}
-        onChange={(e) => onChange(e.target.value)}
-      ></textarea>
-  );
+	return (
+		<textarea
+			style={{ height: 200 }}
+			{...options}
+			id={id}
+			onChange={(e) => onChange(e.target.value)}
+		></textarea>
+	);
 };
 
 export default WPEditor;
